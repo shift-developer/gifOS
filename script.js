bfe72506d09ebad45b0c9c40d477c3c2bdd7a62a
Juan Shift <61192769+shift-developer@users.noreply.github.com>
2020-06-18 00:14:22 -0300

    js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //ENDPOINTS
 const trending = 'gifs/trending';
 const search = 'gifs/search';
+const random = 'gifs/random';
 const trendingSearchs = 'trending/searches'
 
 //REQUEST PARAMETERS
 const limit = '&limit='; //The maximum number of objects to return. (Default: “25”)
 const q = '&q='; //Search query term or phrase.
+const tag = '&tag=' //Filters results by specified tag..
 
-
-async function llamarApi(endpoint, nroLimit = '', stringSearch = '') {
+async function llamarApi(endpoint, nroLimit = '', stringSearch = '', stringTag = '') {
     const API_KEY = '?api_key=CAllNkSvYhmRBlXwfjBCJcvN3CZJ69w5';
     const API_URL = 'http://api.giphy.com/v1/';
     let url = `${API_URL}${endpoint}${API_KEY}`;
@@ -18,25 +19,31 @@ async function llamarApi(endpoint, nroLimit = '', stringSearch = '') {
         if(endpoint == trending) {
             url = url + q + stringSearch;
         }
+    } else if (endpoint == random){
+        url = url + tag + stringTag;
     }
-
     const datos = await fetch(url);
     const datosJSON = await datos.json();
     return datosJSON;
 }
 
 function getTrendingGifs(numeroDeGifs, arrDOM) {
-    const trendingGifsUrls = [];
     llamarApi(trending, numeroDeGifs).then((res) => {
-        for(let gif of res.data){
-            trendingGifsUrls.push(gif.images.downsized.url);
-        }
         for(let i = 0; i < numeroDeGifs; i++){
-            arrDOM[i].src = trendingGifsUrls[i];
+            arrDOM[i].src = res.data[i].images.downsized.url;
         }
-    });
+    });  
+}
+
+function getSugerenciasGifs() {
+    let tags = [];
+    llamarApi(trendingSearchs).then((res) => {
+        tags = res.data;
+        llamarApi(random, )
+    })
     
 }
+
 
 //creamos el array de urls de gifs
 const sugerenciasGifs = document.getElementsByClassName('gif sugerencias');
@@ -48,5 +55,5 @@ getTrendingGifs(4, tendenciasGifs);
 console.log(sugerenciasGifs)
 //console.log();
 
-
+getSugerenciasGifs()
    
